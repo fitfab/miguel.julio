18fe9efe3d3e6c7027f7477142a8d69c63794d3d
Miguel Julio <fitfab@yahoo.com>
2020-09-08 21:20:12 -0400

    add styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    primary: '#FF415C',
+  },
+}
 
 const client = new ApolloClient({
   uri: 'https://api-us-east-1.graphcms.com/v2/ckerhsxkkqv0501yx4zs9bv50/master',
@@ -8,7 +15,13 @@ const client = new ApolloClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider>
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme} >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,32 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
+import styled from 'styled-components'
 import {useQuery} from '@apollo/client'
 import WORK_QUERY from './work.graphql'
 
-const Pill = (techStacks: [string]) => {
+const PillView = styled.strong`
+  display: inline-block;
+  border-radius: 16px;
+  background: ${({theme})=> theme.colors.primary};
+  color: #fff;
+  line-height: 24px;
+  font-size: 16px;
+  font-weight: 300;
+  padding: 0 16px;
+  margin: 0 8px 8px 0;
+  text-transform: uppercase;
+  letter-spacing: .5px;
+`
 
-return techStacks.map((tech, index)=> <em key={index} className={styles.pill}>{tech}</em>)
+const Pill = (techStacks: [string]) => {
+  return techStacks.map((tech, index)=> <PillView key={index} className={styles.pill}>{tech}</PillView>)
 }
 
 const Work = (data: { consultants: any })=> {
   const {consultants} = data
+  const search = "https://www.google.com/search?q="
   return consultants[0].work.map((job, index) => (
-    <a href="https://nextjs.org/docs" className={styles.card} key={index}>
+    <a href={search+job.name} className={styles.card} key={index} target="blank">
       <h2>{job.name} &rarr;</h2>
       <p>{job.description}</p>
       {Pill(job.technology)}
