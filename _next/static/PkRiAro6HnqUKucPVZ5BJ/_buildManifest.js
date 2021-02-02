00566b99a985febb1641784ebfb8a52c48112406
fitfab <fitfab@users.noreply.github.com>
2021-02-02 14:29:21 +0000

    Deploying to gh-pages from  @ 686f5dc73a9662ca82e9b75c4e93ce1f3fea7a13 🚀

---
+++ _next/static/PkRiAro6HnqUKucPVZ5BJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d288a5a5e395e4347d26.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-7b2f861aaf148a14ada4.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
